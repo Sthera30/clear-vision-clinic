8985ea0ec786ebd4a8a10e57c28fc6f8c4185ce1
Sthera30 <tinisthera@gmail.com>
2025-04-16 14:47:02 +0200

    updated f

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -34,18 +34,16 @@ function Login() {
 
                 const res2 = await axios.get(`https://clear-vision-clinic-backend.vercel.app/getUser`, { withCredentials: true })
 
-                console.log(res2.data);
 
 
                 if (res2.data.success) {
                     setUser(res2.data.data.user)
-                    console.log("Hello");
+                    navigate("/")
 
                 }
 
                 else {
                     setUser(null)
-                    console.log("Nope");
 
                 }
 
